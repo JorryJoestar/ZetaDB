1411a33cd332f7899497dac4354462375e90ef44
JorryJoestar <32116800+JorryJoestar@users.noreply.github.com>
2022-02-12 16:42:05 +0000

    ast

--- parser/AST.go
+++ parser/AST.go
@@ -1,129 +1,154 @@
 package parser
 
 //---------------------------------------- AST ----------------------------------------
-type ASTType uint8
+type ASTEnum uint8
 
 const (
-	AST_DDL ASTType = 0
-	AST_DML ASTType = 1
-	AST_DCL ASTType = 2
-	AST_DQL ASTType = 3
+	AST_DDL ASTEnum = 0
+	AST_DML ASTEnum = 1
+	AST_DCL ASTEnum = 2
+	AST_DQL ASTEnum = 3
 )
 
 type AST struct {
-	AstType ASTType
-	Ddl     *DDL
-	Dml     *DML
-	Dcl     *DCL
-	Dql     *DQL
+	Type ASTEnum
+	Ddl  *DDLNode
+	Dml  *DMLNode
+	Dcl  *DCLNode
+	Dql  *DQLNode
 }
 
 //---------------------------------------- DDL ----------------------------------------
-type DDLType uint8
+type DDLEnum uint8
 
 const (
-	DDL_TABLE_CREATE     DDLType = 0
-	DDL_TABLE_DROP       DDLType = 1
-	DDL_TABLE_ALTER_ADD  DDLType = 2
-	DDL_TABLE_ALTER_DROP DDLType = 3
-	DDL_ASSERT_CREATE    DDLType = 4
-	DDL_ASSERT_DROP      DDLType = 5
-	DDL_VIEW_CREATE      DDLType = 6
-	DDL_VIEW_DROP        DDLType = 7
-	DDL_INDEX_CREATE     DDLType = 8
-	DDL_INDEX_DROP       DDLType = 9
+	DDL_TABLE_CREATE     DDLEnum = 0
+	DDL_TABLE_DROP       DDLEnum = 1
+	DDL_TABLE_ALTER_ADD  DDLEnum = 2
+	DDL_TABLE_ALTER_DROP DDLEnum = 3
+	DDL_ASSERT_CREATE    DDLEnum = 4
+	DDL_ASSERT_DROP      DDLEnum = 5
+	DDL_VIEW_CREATE      DDLEnum = 6
+	DDL_VIEW_DROP        DDLEnum = 7
+	DDL_INDEX_CREATE     DDLEnum = 8
+	DDL_INDEX_DROP       DDLEnum = 9
 )
 
 //data definition language
 //CREATE, ALTER, DROP
 //table, view, PSM, trigger, constraint, assert, index
-type DDL struct {
-	DdlType DDLType
-	
+type DDLNode struct {
+	DdlType DDLEnum
+	Table   *TableNode
 }
 
 //table
-type Table struct {
-	TableName           string
+type TableNode struct {
+	TableName     string //used by create, drop, alter
+	AttributeName string // used by alter add, alter drop
+
+	//create table
 	AttributeNameList   []string
-	DomainList          []*Domain
-	ConstraintList      []*Constraint
+	DomainList          []*DomainNode
+	ConstraintList      []*ConstraintNode
 	ConstraintListValid bool
+
+	//alter table add
+	
+
 }
 
 //---------------------------------------- DQL ----------------------------------------
 
 //data query language
 //SELECT
-type DQL struct {
+type DQLNode struct {
 }
 
 //---------------------------------------- DCL ----------------------------------------
-type DCLType uint8
+type DCLEnum uint8
 
 const (
-	DCL_TRANSACTION_BEGIN    DCLType = 0
-	DCL_TRANSACTION_COMMIT   DCLType = 1
-	DCL_TRANSACTION_ROLLBACK DCLType = 2
+	DCL_TRANSACTION_BEGIN    DCLEnum = 0
+	DCL_TRANSACTION_COMMIT   DCLEnum = 1
+	DCL_TRANSACTION_ROLLBACK DCLEnum = 2
 )
 
 //data control language
 //transaction, connection
-type DCL struct {
-	DclType DCLType
+type DCLNode struct {
+	Type DCLEnum
 }
 
 //---------------------------------------- DML ----------------------------------------
-type DMLType uint8
+type DMLEnum uint8
 
 const (
-	DML_INSERT DMLType = 0
-	DML_UPDATE DMLType = 1
-	DML_DELETE DMLType = 2
+	DML_INSERT DMLEnum = 0
+	DML_UPDATE DMLEnum = 1
+	DML_DELETE DMLEnum = 2
 )
 
 //data manipulation language
 //UPDATE, INSERT, DELETE
-type DML struct {
-	DmlType DMLType
+type DMLNode struct {
+	Type DMLEnum
 }
 
 //---------------------------------------- common ----------------------------------------
 
 //domain
-type DomainType uint8
+type DomainEnum uint8
 
 const (
-	DOMAIN_CHAR            DomainType = 0
-	DOMAIN_VARCHAR         DomainType = 1 //VARCHAR(n)
-	DOMAIN_BIT             DomainType = 2 //BIT(n)
-	DOMAIN_BITVARYING      DomainType = 3 //BITVARYING(n)
-	DOMAIN_BOOLEAN         DomainType = 4
-	DOMAIN_INT             DomainType = 5
-	DOMAIN_INTEGER         DomainType = 6
-	DOMAIN_SHORTINT        DomainType = 7
-	DOMAIN_FLOAT           DomainType = 8
-	DOMAIN_REAL            DomainType = 9
-	DOMAIN_DOUBLEPRECISION DomainType = 0
-	DOMAIN_DECIMAL         DomainType = 1 //DECIMAL(n,d)
-	DOMAIN_NUMERIC         DomainType = 2 //NUMERIC(n,d)
-	DOMAIN_DATE            DomainType = 3
-	DOMAIN_TIME            DomainType = 4
+	DOMAIN_CHAR            DomainEnum = 0
+	DOMAIN_VARCHAR         DomainEnum = 1 //VARCHAR(n)
+	DOMAIN_BIT             DomainEnum = 2 //BIT(n)
+	DOMAIN_BITVARYING      DomainEnum = 3 //BITVARYING(n)
+	DOMAIN_BOOLEAN         DomainEnum = 4
+	DOMAIN_INT             DomainEnum = 5
+	DOMAIN_INTEGER         DomainEnum = 6
+	DOMAIN_SHORTINT        DomainEnum = 7
+	DOMAIN_FLOAT           DomainEnum = 8
+	DOMAIN_REAL            DomainEnum = 9
+	DOMAIN_DOUBLEPRECISION DomainEnum = 0
+	DOMAIN_DECIMAL         DomainEnum = 1 //DECIMAL(n,d)
+	DOMAIN_NUMERIC         DomainEnum = 2 //NUMERIC(n,d)
+	DOMAIN_DATE            DomainEnum = 3
+	DOMAIN_TIME            DomainEnum = 4
 )
 
-type Domain struct {
-	Type DomainType
+type DomainNode struct {
+	Type DomainEnum
 	N    int
 	D    int
 }
 
 //(TableName.)AttributeName
-type AttriNameOptTableName struct {
+type AttriNameOptTableNameNode struct {
 	TableNameValid bool
 	AttributeName  string
 	TableName      string
 }
 
 //constraint
-type Constraint struct {
+type ConstraintNode struct {
+}
+
+//elementary value
+type ElementaryValueEnum uint8
+
+const (
+	ELEMENTARY_VALUE_INT     ElementaryValueEnum = 0
+	ELEMENTARY_VALUE_FLOAT   ElementaryValueEnum = 1
+	ELEMENTARY_VALUE_STRING  ElementaryValueEnum = 2
+	ELEMENTARY_VALUE_BOOLEAN ElementaryValueEnum = 3
+)
+
+type ElementaryValueNode struct {
+	Type         ElementaryValueEnum
+	IntValue     int
+	FloatValue   float64
+	StringValue  string
+	BooleanValue bool
 }
