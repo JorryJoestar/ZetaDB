3b54a85836d945f05b6670436af42210317bb99f
JorryJoestar <32116800+JorryJoestar@users.noreply.github.com>
2022-02-22 10:47:25 +0000

    token update

--- parser/token.h
+++ parser/token.h
@@ -1,191 +1,191 @@
 enum {
 //createTableStmt
-    CREATE          = 232,
-    TABLE           = 225,
+    CREATE          = 200,
+    TABLE           = 201,
 
 //dropTableStmt
-    DROP            = 226,
+    DROP            = 202,
 
 //alterTableStmt
-    ALTER           = 227,
-    ADD             = 228,
+    ALTER           = 203,
+    ADD             = 204,
 
 //createAssertStmt
-    ASSERTION       = 301,
+    ASSERTION       = 205,
 
 //createViewStmt
-    VIEW            = 317,
-    AS              = 242,
+    VIEW            = 206,
+    AS              = 207,
 
 //createIndexStmt
-    INDEX           = 318,
-    ON              = 234,
+    INDEX           = 208,
+    ON              = 209,
 
 //createTriggerStmt
-    TRIGGER         = 302,
-    REFERENCING     = 315,
-    BEFORE          = 303,
-    UPDATE          = 279,
-    OF              = 304,
-    AFTER           = 305,
-    INSTEAD         = 316,
-    INSERT          = 306,
-    DELETE          = 278,
-    OLD             = 307,
-    ROW             = 308,
-    NEW             = 309,
-    FOR             = 310,
-    EACH            = 311,
-    STATEMENT       = 312,
-    WHEN            = 313,
-    BEGINTOKEN      = 283,  //BEGIN
-    END             = 314,
+    TRIGGER         = 210,
+    REFERENCING     = 211,
+    BEFORE          = 212,
+    UPDATE          = 213,
+    OF              = 214,
+    AFTER           = 215,
+    INSTEAD         = 216,
+    INSERT          = 217,
+    DELETE          = 218,
+    OLD             = 219,
+    ROW             = 220,
+    NEW             = 221,
+    FOR             = 222,
+    EACH            = 223,
+    STATEMENT       = 224,
+    WHEN            = 225,
+    BEGINTOKEN      = 226,  //BEGIN
+    END             = 227,
 
 //aggregation
-    STAR            = 245,  //*
-    AVG             = 259,
-    MIN             = 260,
-    MAX             = 261,
-    COUNT           = 262,
-    SUM             = 263,
-    DISTINCT        = 239,
+    STAR            = 228,  //*
+    AVG             = 229,
+    MIN             = 230,
+    MAX             = 231,
+    COUNT           = 232,
+    SUM             = 233,
+    DISTINCT        = 234,
 
 //constraint
-    UNIQUE            = 231,
-    PRIMARYKEY        = 230,  //PRIMARY KEY
-    CHECK             = 289,
-    FOREIGNKEY        = 291,
-    REFERENCES        = 290,
-    NOT_DEFERRABLE           = 292,  //NOT DEFERRABLE
-    DEFERED_DEFERRABLE       = 293,  //DEFERRABLE INITIALLY DEFERRED
-    IMMEDIATE_DEFERRABLE     = 294,  //DEFERRABLE INITIALLY IMMEDIATE
-    UPDATE_NULL      = 295,  //ON UPDATE SET NULL
-    UPDATE_CASCADE   = 296,  //ON UPDATE SET CASCADE
-    DELETE_NULL      = 297,  //ON DELETE SET NULL
-    DELETE_CASCADE   = 298,  //ON DELETE SET CASCADE
-    CONSTRAINT      = 288,
-    DEFAULT         = 229,
+    UNIQUE            = 235,
+    PRIMARYKEY        = 236,  //PRIMARY KEY
+    CHECK             = 237,
+    FOREIGNKEY        = 238,
+    REFERENCES        = 239,
+    NOT_DEFERRABLE           = 240,  //NOT DEFERRABLE
+    DEFERED_DEFERRABLE       = 241,  //DEFERRABLE INITIALLY DEFERRED
+    IMMEDIATE_DEFERRABLE     = 242,  //DEFERRABLE INITIALLY IMMEDIATE
+    UPDATE_NULL      = 243,  //ON UPDATE SET NULL
+    UPDATE_CASCADE   = 244,  //ON UPDATE SET CASCADE
+    DELETE_NULL      = 245,  //ON DELETE SET NULL
+    DELETE_CASCADE   = 246,  //ON DELETE SET CASCADE
+    CONSTRAINT      = 247,
+    DEFAULT         = 248,
 
 //condition
-    AND             = 243,
-    OR              = 244,
+    AND             = 249,
+    OR              = 250,
 
 //predicate
-    LIKE            = 258,
+    LIKE            = 251,
     NOT             = 252,
     IN              = 253,
     ALL             = 254,
     ANY             = 255,
     IS              = 256,
-    EXISTS          = 277,
+    EXISTS          = 257,
 
 //expression
-    SUBTRACT        = 271,  //-
-    DIVISION        = 272,  ///
-    CONCATENATION   = 273,  //||
-    PLUS            = 274,  //+
+    SUBTRACT        = 258,  //-
+    DIVISION        = 259,  ///
+    CONCATENATION   = 260,  //||
+    PLUS            = 261,  //+
 
-    NOTEQUAL        = 246,  // <>
-    LESS            = 247,  // <
-    GREATER         = 248,  // >
-    LESSEQUAL       = 249,  // <=
-    GREATEREQUAL    = 250,  // >=
-    EQUAL           = 251,  // =
+    NOTEQUAL        = 262,  // <>
+    LESS            = 263,  // <
+    GREATER         = 264,  // >
+    LESSEQUAL       = 265,  // <=
+    GREATEREQUAL    = 266,  // >=
+    EQUAL           = 267,  // =
 
 //elementary value
-    INTVALUE          = 204,  //int value
-    FLOATVALUE        = 205,  //float value
-    STRINGVALUE       = 206,  //string surrounded by single or double quotation marks
-    BOOLVALUE         = 551,  //BOOLVALUE true/false
+    INTVALUE          = 268,  //int value
+    FLOATVALUE        = 269,  //float value
+    STRINGVALUE       = 270,  //string surrounded by single or double quotation marks
+    BOOLVALUE         = 271,  //BOOLVALUE true/false
 
 //attriNameOptionTableName
-    DOT               = 241,  //.
+    DOT               = 272,  //.
 
 //domain
-    CHAR            = 224,
-    VARCHAR         = 210,  //VARCHAR(n)
-    BIT             = 211,  //BIT(n)
-    BITVARYING      = 212,  //BITVARYING(n)
-    BOOLEAN         = 213,
-    INT             = 214,
-    INTEGER         = 215,
-    SHORTINT        = 216,
-    FLOAT           = 217,
-    REAL            = 218,
-    DOUBLEPRECISION = 219,
-    DECIMAL         = 220,  //DECIMAL(n,d)
-    NUMERIC         = 221,  //NUMERIC(n,d)
-    DATE            = 222,
-    TIME            = 223,
+    CHAR            = 273,
+    VARCHAR         = 274,  //VARCHAR(n)
+    BIT             = 275,  //BIT(n)
+    BITVARYING      = 276,  //BITVARYING(n)
+    BOOLEAN         = 277,
+    INT             = 278,
+    INTEGER         = 279,
+    SHORTINT        = 280,
+    FLOAT           = 281,
+    REAL            = 282,
+    DOUBLEPRECISION = 283,
+    DECIMAL         = 284,  //DECIMAL(n,d)
+    NUMERIC         = 285,  //NUMERIC(n,d)
+    DATE            = 286,
+    TIME            = 287,
 
 //psm
-    CALL            = 550,
-    ELSEIF          = 577,
-    THEN            = 575,
-    IF              = 574,
-    ELSE            = 576,
-    CURSOR          = 560,
-    DO              = 569,
-    RETURN          = 561,
-    SET             = 280,
-    OUT             = 554,
-    INOUT           = 555,
-    DECLARE         = 557,
-    FUNCTION        = 552,
-    RETURNS         = 553,
-    PROCEDURE       = 556,
+    CALL            = 288,
+    ELSEIF          = 289,
+    THEN            = 290,
+    IF              = 291,
+    ELSE            = 292,
+    CURSOR          = 293,
+    DO              = 294,
+    RETURN          = 295,
+    SET             = 296,
+    OUT             = 297,
+    INOUT           = 298,
+    DECLARE         = 299,
+    FUNCTION        = 300,
+    RETURNS         = 301,
+    PROCEDURE       = 302,
 
 //delete
-    FROM            = 233,
-    WHERE           = 235,
+    FROM            = 303,
+    WHERE           = 304,
 
 //insert
-    INSERTINTO      = 281,  //INSERT INTO
-    VALUES          = 282,
+    INSERTINTO      = 305,  //INSERT INTO
+    VALUES          = 306,
 
 //dcl
-    START           = 284,
-    TRANSACTION     = 285,
-    COMMIT          = 286,
-    ROLLBACK        = 287,
-    SHOW            = 600,
-    TABLES          = 601,
-    ASSERTIONS      = 602,
-    VIEWS           = 603,
-    INDEXS          = 604,
-    TRIGGERS        = 605,
-    FUNCTIONS       = 606,
-    PROCEDURES      = 607,
-    USER            = 608,
-    PASSWORD        = 609,
-    PASSWORDS       = 610,
-    CONNECT         = 319, 
+    START           = 307,
+    TRANSACTION     = 308,
+    COMMIT          = 309,
+    ROLLBACK        = 310,
+    SHOW            = 311,
+    TABLES          = 312,
+    ASSERTIONS      = 313,
+    VIEWS           = 314,
+    INDEXS          = 315,
+    TRIGGERS        = 316,
+    FUNCTIONS       = 317,
+    PROCEDURES      = 318,
+    USER            = 319,
+    PASSWORD        = 320,
+    PASSWORDS       = 321,
+    CONNECT         = 322, 
 
 //dql
-    DESC            = 275,
-    ASC             = 276,
-    JOIN            = 264,
-    NATURAL         = 265,
-    FULL            = 266,
-    LEFT            = 267,
-    RIGHT           = 268,
-    OUTER           = 269,
-    CROSS           = 270,
-    SELECT          = 240,
-    GROUPBY         = 236,  //GROUP BY
-    HAVING          = 237,
-    ORDERBY         = 238,  //ORDER BY
-    LIMIT           = 700,
-    UNION           = 701,
-    DIFFERENCE      = 702,
-    INTERSECTION    = 703,
+    DESC            = 323,
+    ASC             = 324,
+    JOIN            = 325,
+    NATURAL         = 326,
+    FULL            = 327,
+    LEFT            = 328,
+    RIGHT           = 329,
+    OUTER           = 330,
+    CROSS           = 331,
+    SELECT          = 332,
+    GROUPBY         = 333,  //GROUP BY
+    HAVING          = 334,
+    ORDERBY         = 335,  //ORDER BY
+    LIMIT           = 336,
+    UNION           = 337,
+    DIFFERENCE      = 338,
+    INTERSECTION    = 339,
 
 //public
-    NULLMARK          = 257,  //NULL
-    LPAREN            = 200,  //(
-    RPAREN            = 201,  //)
-    SEMICOLON         = 202,  //;
-    COMMA             = 203,  //,
-    ID                = 207,  //begin with non-number character
-    TABLEDOTATTRIBUTE = 209, //tableName.attributeName
+    NULLMARK          = 340,  //NULL
+    LPAREN            = 341,  //(
+    RPAREN            = 342,  //)
+    SEMICOLON         = 343,  //;
+    COMMA             = 344,  //,
+    ID                = 345,  //begin with non-number character
+    TABLEDOTATTRIBUTE = 346, //tableName.attributeName
 };
