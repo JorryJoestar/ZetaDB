d863bc265498e14b45a9845d3f117fd50f935efa
JorryJoestar <32116800+JorryJoestar@users.noreply.github.com>
2022-02-27 11:16:47 +0000

    storage

--- storage/dataBuffer.go
+++ storage/dataBuffer.go
@@ -1,17 +1,43 @@
 package storage
 
+//TODO
 type dataBuffer struct {
 
 	//page container
-	buffer map[int]dataBuffer
+	buffer map[uint32]*dataBuffer
+
+	//mapping from file pageId to buffer index
+	mapper map[uint32]uint32
+
+	//record all empty buffer slot index
+	bufferSlots []uint32
 }
 
-//
-func (db *dataBuffer) GetDataPageByPageId(pageId int) dataPage {
-	dp := dataPage{}
-	return dp
+//fetch a data page from data buffer by its pageId
+func (db *dataBuffer) GetDataPageByPageId(pageId uint32) (*dataPage, error) {
+	dp := &dataPage{}
+	return dp, nil
 }
 
-func (db *dataBuffer) InsertDataPage(dataPage) {
+//fetch a data page from data buffer by its buffer index
+func (db *dataBuffer) GetDataPageByBufferIndex(pos uint32) (*dataPage, error) {
+	dp := &dataPage{}
+	return dp, nil
+}
 
+//insert a data page into the buffer, then recording the mapping relation between pageId and buffer index
+func (db *dataBuffer) InsertDataPage(*dataPage) error {
+	return nil
+}
+
+//convert pageId to dataBufferId
+//if no such pageId in mapper, throw error
+func (db *dataBuffer) PageIdToDataBufferId(pageId uint32) (uint32, error) {
+	return 0, nil
+}
+
+//convert dataBufferId to pageId
+//if no such dataBufferId in mapper, throw error
+func (db *dataBuffer) DataBufferIdToPageId(dbId uint32) (uint32, error) {
+	return 0, nil
 }
