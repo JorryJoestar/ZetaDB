3ce189d1ffbd977639db1760e7759dd93c8a3f75
JorryJoestar <32116800+JorryJoestar@users.noreply.github.com>
2022-03-05 19:08:05 +0000

    dataBuffer

--- storage/dataBuffer.go
+++ storage/dataBuffer.go
@@ -3,30 +3,23 @@ package storage
 import (
 	. "ZetaDB/container"
 	. "ZetaDB/utility"
-	"errors"
 )
 
 type dataBuffer struct {
 
-	//page container, key is bufferId
-	buffer map[int]*dataPage
+	//page container, key is called bufferId
+	buffer map[uint32]*dataPage
 
-	//mapping from file pageId to bufferId
-	mapper map[uint32]int
+	//mapping from pageId to bufferId
+	mapper map[uint32]uint32
 
-	//bufferId is from 1 to dataBufferSize
+	//bufferId is from 1 to DEFAULT_DATA_BUFFER_SIZE
 	//record all empty slot bufferId
-	bufferSlots []int
-
-	//fetch from systemParameter.go when this buffer is initiallized
-	dataBufferSize int
-
-	//fetch from systemParameter.go when this buffer is initiallized
-	dataPageSize int
+	bufferSlots []uint32
 }
 
-//in order to fetch a dataBuffer, call this function
-func GetDataBuffer() *dataBuffer {
+//in order to create a new dataBuffer, call this function
+func NewDataBuffer() *dataBuffer {
 	db := &dataBuffer{}
 	db.buffer = make(map[int]*dataPage)
 	db.mapper = make(map[uint32]int)
@@ -41,9 +34,10 @@ func GetDataBuffer() *dataBuffer {
 	return db
 }
 
-//fetch a data page from data buffer by its pageId, if it is not in buffer, fetch it from disk and buffer it
-//TODO
-func (db *dataBuffer) GetDataPageByPageId(pageId uint32, schema *Schema, ioM *IOManipulator) (*dataPage, error) {
+//fetch a data page from data buffer by its pageId
+//throw error if pageId invalid
+//throw error if data page with id pageId is not in this buffer
+func (db *dataBuffer) DataBufferFetchPage(pageId uint32) (*dataPage, error) {
 
 	//fetch bufferId from mapper
 	bufferId, err1 := db.PageIdToBufferId(pageId)
@@ -67,72 +61,35 @@ func (db *dataBuffer) GetDataPageByPageId(pageId uint32, schema *Schema, ioM *IO
 	dataPage.MarkDataPage()
 
 	return dataPage, err2
-
 }
 
-//fetch a data page from data buffer by its bufferId
-func (db *dataBuffer) GetDataPageByBufferId(bufferId int) (*dataPage, error) {
-
-	page, err := db.buffer[bufferId]
-
-	if !err { //can not find corresponding page from buffer
-		return nil, errors.New("bufferId invalid")
-	}
-
-	//mark this page
-	page.MarkDataPage()
-
-	return page, nil
-}
-
-//insert a data page into the buffer, notice the page is newly created so it need to be pushed into disk
-func (db *dataBuffer) InsertDataPage(*dataPage) error {
+//insert a data page into the buffer
+//throw error if data page with the same pageId is already in this buffer
+//throw error if this buffer is full at present
+func (db *dataBuffer) DataBufferInsertDataPage(*dataPage) error {
 	return nil
 }
 
-//delete a data page from the buffer, also delete it from the disk
-func (db *dataBuffer) DeleteDataPage() error {
+//delete a data page from the buffer
+//throw error if this page is not in this buffer
+//throw error if this page is modified
+func (db *dataBuffer) DataBufferDeleteDataPage(pageId uint32) error {
 	return nil
+}
+
+//pick a page that is not used recently, return it, but it is not deleted from this buffer
+//evict order: (unmarked,unmodified) (marked,unmodified) (unmarked, modified) (marked, modified)
+//throw error if this buffer is empty at present
+func (db *dataBuffer) DataBufferEvictDataPage() (*dataPage, error) {
+	return 0
 }
 
 //test if buffer is full
-func (db *dataBuffer) BufferIsFull() bool {
+func (db *dataBuffer) DataBufferIsFull() bool {
 	return len(db.bufferSlots) == 0
 }
 
 //test if buffer is empty
-func (db *dataBuffer) BufferIsEmpty() bool {
+func (db *dataBuffer) DataBufferIsEmpty() bool {
 	return len(db.bufferSlots) == db.dataBufferSize
-}
-
-//TODO
-//pick a page that is not used recently, if it is modified, push it into the disk, empty the slot and return bufferId
-func (db *dataBuffer) EvictDataPage() int {
-	return 0
-}
-
-//convert pageId to bufferId
-//if no such pageId in mapper, throw error
-func (db *dataBuffer) PageIdToBufferId(pageId uint32) (int, error) {
-	bufferId := db.mapper[pageId]
-
-	if bufferId == 0 { //no corresponding bufferId mapping from this pageId
-		return 0, errors.New("pageId invalid")
-	}
-
-	return bufferId, nil
-}
-
-//convert bufferId to pageId
-//if no such bufferId in mapper, throw error
-func (db *dataBuffer) BufferIdToPageId(bufferId int) (uint32, error) {
-	p := db.buffer[bufferId]
-
-	if p == nil { //empty slot from bufferId
-		return 0, errors.New("bufferId invalid")
-	}
-
-	//get pageId from this dataPage
-	pageId := p.DpGetPageId()
-	return pageId, nil
 }
