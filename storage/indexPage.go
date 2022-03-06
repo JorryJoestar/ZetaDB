ccc2d881980bf3e0d4b792a3f0b4d9bab5a42e84
JorryJoestar <32116800+JorryJoestar@users.noreply.github.com>
2022-03-06 22:37:55 +0000

    indexPage

--- storage/indexPage.go
+++ storage/indexPage.go
@@ -68,6 +68,7 @@ package storage
 		2: this is a leaf node
 
 	~index/dataPageId X
+		-uint32, 4 bytes
 		pointer to a dataPage which contains tuple with element X
 		or
 		pointer to an indexPage mode 1, for multiple-key indexes
@@ -75,6 +76,7 @@ package storage
 		pointer to an indexPage mode 3, for duplicated tuples
 
 	~record type X
+		-int32, 4 bytes
 		1: index/dataPageId X is pointer to an indexPage mode 1
 		2: index/dataPageId X is pointer to a dataPage in data file
 		3: index/dataPageId X is pointer to an indexPage mode 3
@@ -97,11 +99,77 @@ package storage
 		3: this is a duplicated node
 
 	~dataPageNum x
-		number of dataPages that contain element x
+		-int32, 4 bytes
+		-number of dataPages that contain element x
 
 	~dataPageId xn
 		pageId in data file which contains element x
 
 */
 
-type indexPage struct{}
+type indexPage struct {
+	indexPageId      uint32
+	mode             int32
+	elementType      int32
+	elementNum       int32
+	elements         [][]byte
+	pointerPageId    []uint32   //valid for mode 1
+	indexDataPageIds []uint32   //valid for mode 2
+	recordType       []int32    //valid for mode 2
+	dataPageNums     []int32    // valid for mode 3
+	dataPageIds      [][]uint32 // valid for mode 3
+}
+
+//create indexPage from bytes
+//throw error if bytes length invalid
+func NewIndexPageFromBytes(bytes []byte) (*indexPage, error) {
+	return nil, nil
+}
+
+//create indexPage
+func NewIndexPage(indexPageId uint32, mode int32, elementType int32) (*indexPage, error) {
+	return nil, nil
+}
+
+//convert this index page to byte slice, ready to push into disk
+func (ip *indexPage) IndexPageToBytes() []byte {
+	return nil
+}
+
+//search the next layer, return indexPageId of the corresponding next layer node
+//throw error if this page is not an internal node
+//throw error if elementValue byte number is invalid
+func (ip *indexPage) IndexPageInternalSearch(elementValue []byte) (uint32, error) {
+	return 0, nil
+}
+
+//search related record according to elementValue, return index/dataPageId and recordType
+//throw error if this page is not a leaf node
+//throw error if elementValue byte number is invalid
+//throw error if no element meets requirement
+func (ip *indexPage) IndexPageLeafSearch(elementValue []byte) (uint32, int32, error) {
+	return 0, 0, nil
+}
+
+//search related dataPageIds where tuples whose element value is elementValue
+//throw error if this page is not a duplicated node
+//throw error if elementValue byte number is invalid
+//throw error if no element meets requirement
+func (ip *indexPage) IndexPageDuplicatedSearch(elementValue []byte) ([]uint32, error) {
+	return nil, nil
+}
+
+//mode getter
+func (ip *indexPage) IndexPageGetMode() int32 {
+	return 0
+}
+
+//return max entry number this page can contain
+//for internal node, it is max number of pointers it can have (M)
+//for leaf node, it is max number of elements it can have (L)
+//throw error if this is a duplicated page
+func (ip *indexPage) IndexPageMaxNum() (int32, error) {
+	return 0, nil
+}
+
+//TODO !!! mode 3 structure need to be modified
