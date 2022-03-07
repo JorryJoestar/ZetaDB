a109a8fa4b7ce0185f676a39c8cfdd5e41119d79
JorryJoestar <32116800+JorryJoestar@users.noreply.github.com>
2022-03-07 15:59:04 +0000

    log

--- storage/log.go
+++ storage/log.go
@@ -12,25 +12,28 @@ import (
    ----------------------------------------------------------------
 
 	~fileMode
+		-uint8, 1 byte
 		-1: from logPageId to data file at objectPageId
 		-2: from logPageId to index file at objectPageId
 
 	~logPageId
-		the snapshot id in log file
+		-uint32, 4 bytes
+		-the snapshot id in log file
 
 	~objectPageId
-		the id where this snapshot page should be stored at
+		-uint32, 4 bytes
+		-the id where this snapshot page should be stored at
 */
 
 type log struct {
-	fileMode     int32
+	fileMode     uint8
 	logPageId    uint32
 	objectPageId uint32
 }
 
 //create a new log
 //throw error if fileMode is not 1 or 2
-func NewLog(fileMode int32, logPageId uint32, objectPageId uint32) (*log, error) {
+func NewLog(fileMode uint8, logPageId uint32, objectPageId uint32) (*log, error) {
 	//throw error if fileMode is not 1 or 2
 	if fileMode != 1 && fileMode != 2 {
 		return nil, errors.New("fileMode invalid")
@@ -45,16 +48,16 @@ func NewLog(fileMode int32, logPageId uint32, objectPageId uint32) (*log, error)
 }
 
 //create a new log from bytes
-//throw error if bytes length is not 12
+//throw error if bytes length is not 9
 func NewLogFromBytes(bytes []byte) (*log, error) {
-	//throw error if bytes length is not 12
-	if len(bytes) != 12 {
+	//throw error if bytes length is not 9
+	if len(bytes) != 9 {
 		return nil, errors.New("byte slice length invalid")
 	}
 
-	fileMode, _ := BytesToINT(bytes[0:4])
-	logPageId, _ := BytesToUint32(bytes[4:8])
-	objectPageId, _ := BytesToUint32(bytes[8:12])
+	fileMode := bytes[0]
+	logPageId, _ := BytesToUint32(bytes[1:5])
+	objectPageId, _ := BytesToUint32(bytes[5:9])
 
 	l := &log{
 		fileMode:     fileMode,
@@ -68,7 +71,7 @@ func NewLogFromBytes(bytes []byte) (*log, error) {
 func (l *log) LogToBytes() []byte {
 	var bytes []byte
 
-	bytes = append(bytes, INTToBytes(l.fileMode)...)
+	bytes = append(bytes, l.fileMode)
 	bytes = append(bytes, Uint32ToBytes(l.logPageId)...)
 	bytes = append(bytes, Uint32ToBytes(l.objectPageId)...)
 
@@ -76,7 +79,7 @@ func (l *log) LogToBytes() []byte {
 }
 
 //fileMode getter
-func (l *log) LogGetFileMode() int32 {
+func (l *log) LogGetFileMode() uint8 {
 	return l.fileMode
 }
 
